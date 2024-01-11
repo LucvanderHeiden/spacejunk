5a8968a484fcd781260a21b8e29b9d484c04b49b
Luc van der Heiden <lucvdheiden@hotmail.com>
2024-01-11 17:59:06 +0100

    small fixes

--- script.js
+++ script.js
@@ -4,15 +4,14 @@ const debrisImages = [
   "images/debris1.png",
   "images/debris2.png",
   "images/debris3.png",
-  // Add more image sources as needed
 ];
 
 function getRandomImage() {
-  // 80% chance of selecting a "rock" image
+  // 95% chance of selecting a "rock" image
   if (Math.random() < 0.95) {
     return "images/rock.webp";
   } else {
-    // 20% chance of selecting any image, including "rock"
+    // 5% chance of selecting any image, including "rock"
     return debrisImages[Math.floor(Math.random() * debrisImages.length)];
   }
 }
@@ -28,15 +27,15 @@ function createDebris() {
   debris.style.left = Math.random() * 100 + "%";
   debris.style.top = Math.random() * 100 + "%";
   debris.style.transform = `rotate(${Math.random() * 360}deg)`;
-  debris.style.zIndex = Math.floor(Math.random() * 3) + 1; // Adjust the number of layers as needed
+  debris.style.zIndex = Math.floor(Math.random() * 3) + 1; // Number of layers
 
   debrisContainer.appendChild(debris);
 
   // Ensure that the animation starts only after the element is added to the DOM
   setTimeout(() => {
     debris.style.animation = `debrisAnimation 20s linear`;
-    debris.addEventListener("animationend", () => debris.remove()); // Remove debris after animation
+    debris.addEventListener("animationend", () => debris.remove()); // Removes debris after animation
   }, 0);
 }
 
-setInterval(createDebris, 100); // Adjust the interval based on your preference
+setInterval(createDebris, 100); // Interval in miliseconds
